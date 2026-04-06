ac2c1ffcb4bde1d421939856e4fb20bdadd84d4a
n4n5 <its.just.n4n5@gmail.com>
2026-04-06 17:31:58 -0600

    add bladvak cli

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+fn main(){
+    println!(r#"
+#!/bin/bash
+# {}@{}
+
+repo="https://raw.githubusercontent.com/Its-Just-Nans/bladvak/main/assets"
+mkdir -p .github/workflows
+for file in pages.yml release.yml rust.yml typos.yml; do
+  curl -L -o ".github/workflows/$file" \
+  "$repo/$file"
+done
+curl -L -O "$repo/rust-toolchain"
+curl -L -O "$repo/index.html"
+sed -i "s/BLADVAK_APP/$(basename "$PWD")/g" index.html
+"#, env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
+}
