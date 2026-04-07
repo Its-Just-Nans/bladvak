08601f7c4826c9c33ffbfe01984a8c61738f0308
n4n5 <its.just.n4n5@gmail.com>
2026-04-06 18:03:22 -0600

    add

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ for file in pages.yml release.yml rust.yml typos.yml; do
   curl -L -o ".github/workflows/$file" \
   "$repo/$file"
 done
+curl -L -O "$repo/.gitignore"
 curl -L -O "$repo/rust-toolchain"
 curl -L -O "$repo/index.html"
 sed -i "s/BLADVAK_APP/$(basename "$PWD")/g" index.html
